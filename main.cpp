72d3cbda07cf859f244b82d25502eae6c61bb666
Maxim345525 <maximmaksimchyk@gmail.com>
2023-03-10 21:27:49 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,21 @@
-d
+#include<iostream>
+using namespace std;
+int my_strlen(char* str)
+{
+	size_t i = 0;
+	for(; str[i] != '\0'; i++)
+	{
+	}
+	return i;
+}
+int main()
+{
+	char str[1000];
+	cout << "Input text->";
+	gets_s(str);
+	str[' '] = '   ';
+	for (size_t i = 0; i < my_strlen(str) + 1; i++)
+	{
+		cout << str[i] << '\t';
+	}
+}
