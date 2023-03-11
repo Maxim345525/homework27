b1473849e84a290a71a550bb56ac2b6d9ee96a0e
Maxim345525 <maximmaksimchyk@gmail.com>
2023-03-11 20:42:19 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
-d
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+    char str[100];
+    int len, word = 0;
+    cout << "Input text: ";
+    cin.getline(str, 100);
+    len = strlen(str);
+    for (int i = 0; i < len; i++) {
+        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' ')) 
+        {
+            word++;
+        }
+    }
+    cout << " Number of words: " << word << endl;
+    return 0;
+}
