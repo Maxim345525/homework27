6228136082efe4e7511f0cd5f10f35a4ac6fdf51
Maxim345525 <maximmaksimchyk@gmail.com>
2023-03-12 12:24:12 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
-d
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+	char str[100];
+	int  palindrom = 0;
+	int len = 0;
+	cout << "Input text:";
+	cin.getline(str, 100);
+	len = strlen(str);
+	for (int i = 0; i < len; i++)
+	{
+		if ((palindrom[i] != palindrom [len - i - 1])
+		{
+			cout << "Palindrom" << endl;
+
+		}
+		else
+		{
+			cout << "Ne palindrom" << endl;
+		}
+	}
+}
